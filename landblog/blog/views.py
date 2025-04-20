2e828a6565014226dca4779d26a9561e6bc9118a
licailong <3033584702@qq.com>
2025-04-20 18:00:36 +0800

    添加了首页文章展示

--- landblog/blog/views.py
+++ landblog/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from blog.models import Article
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    post_list = Article.objects.all().order_by('-created_at')
+
+    context = {
+        'post_list': post_list,
+    }
+
+    return render(request, 'index.html', context=context)
